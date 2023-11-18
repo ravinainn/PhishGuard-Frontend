d9e6eec346793374fc9e2310bcf8627c3d6061a5
ravinainn <nain.ravi12345@gmail.com>
2023-11-18 10:47:15 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,61 @@
-import './App.css'
-import { SideBar } from './components'
-import { GlobalStyles } from './styles/Global'
-import { ThemeProvider } from 'styled-components'
-import { Link, createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom'
-import RootLayout from './layouts/RootLayout'
-import { DashBoard, InfoAnalysisPage, DomainAnalysisPage, WebContentAnalysisPage } from './Pages'
+import "./App.css";
+import { SideBar } from "./components";
+import { GlobalStyles } from "./styles/Global";
+import { ThemeProvider } from "styled-components";
+import {
+  Link,
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+import {
+  DashBoard,
+  InfoAnalysisPage,
+  DomainAnalysisPage,
+  WebContentAnalysisPage,
+} from "./Pages";
 
 const theme = {
   palette: {
-    primary: '#19A7CE',
-    secondary: '#146C94',
-    white: '#FFFFFF',
-    black: '#000000',
-    gray: '#4B4B4B',
-    lightgray: '#828282',
+    primary: "#19A7CE",
+    secondary: "#146C94",
+    white: "#FFFFFF",
+    black: "#000000",
+    gray: "#4B4B4B",
+    lightgray: "#828282",
   },
   indicator: {
-    warning: '#FE1405',
-    clean: '#00DA58',
-    sus: '#A18800',
+    warning: "#FE1405",
+    clean: "#00DA58",
+    sus: "#A18800",
   },
-  bg: '#F6F1F1',
-  placeholder : '#5E5E5E;'
-}
+  bg: "#F6F1F1",
+  placeholder: "#5E5E5E;",
+};
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<RootLayout />} >
-      <Route index element={ <DashBoard /> } />
-      <Route path='info' element={ <InfoAnalysisPage /> } />
-      <Route path='domain' element={ <DomainAnalysisPage /> } />
-      <Route path='web-content' element={ <WebContentAnalysisPage /> } />
+    <Route path="/" element={<RootLayout />}>
+      <Route path="/home" element={<DashBoard />} />
+      <Route index element={<DashBoard />} />
+      <Route path="info" element={<InfoAnalysisPage />} />
+      <Route path="domain" element={<DomainAnalysisPage />} />
+      <Route path="web-content" element={<WebContentAnalysisPage />} />
     </Route>
   )
-)
+);
 
 function App() {
-
   return (
-    <ThemeProvider theme={theme} >
-    <>
-      <GlobalStyles />
-      <RouterProvider router={router} />
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <RouterProvider router={router} />
+      </>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AnalysedURLSection/AnalysedURLSection.jsx
+++ src/components/AnalysedURLSection/AnalysedURLSection.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Card, Flex, Subtitle } from '@tremor/react'
+import React from "react";
+import styled from "styled-components";
+import { Card, Flex, Subtitle } from "@tremor/react";
 
 const AnalysedURLSection = () => {
   return (
     <MonitorSection>
-     <Flex>
-      <StyledCard>
-        <Subtitle>social-media-account-check.net</Subtitle>
+      <Flex>
+        <StyledCard>
+          <Subtitle>social-media-account-check.net</Subtitle>
         </StyledCard>
         <InputButton>Add URL to Monitoring</InputButton>
-     </Flex>
+      </Flex>
     </MonitorSection>
-  )
-}
+  );
+};
 
-export default AnalysedURLSection
+export default AnalysedURLSection;
 
 const MonitorSection = styled.div`
   margin-top: 2.88rem;
-`
+`;
 
 const StyledCard = styled(Card)`
   border-radius: 1rem;
   width: 59%;
   padding: 0.8rem 0 0.8rem 1.5rem;
-`
+`;
 
 const InputButton = styled.button`
   width: 40%;
@@ -37,4 +37,4 @@ const InputButton = styled.button`
   background-color: ${({ theme }) => theme.palette.secondary};
   padding: 0.45rem;
   border-radius: 1rem;
-`
+`;
